a2d42e800d6be3b0cd92b0c9351468a20c43c750
mfl4 <mhdfarhanl6267@gmail.com>
2023-12-16 00:08:08 +0700

    feat: Implement CRUD operations in notes controller

--- src/controllers/notes.js
+++ src/controllers/notes.js
@@ -1,61 +1,97 @@
-// const Note = require('../models/note')
-const responses = require('../constants/responses')
-const apiResponse = require('../helpers/response-helper')
+const Note = require("../models/note");
+const responses = require("../constants/responses");
+const apiResponse = require("../helpers/response-helper");
 
 const getNotes = async (req, res) => {
-  try {
-    /**
-     * @todo Write your code here
-     */
-  } catch (error) {
-    return apiResponse(res, responses.error.code, 'Error retrieving notes')
-  }
-}
+    try {
+        const notes = await Note.findAll();
+        return apiResponse(
+            res,
+            responses.success.code,
+            "Notes retrieved successfully",
+            notes
+        );
+    } catch (error) {
+        return apiResponse(res, responses.error.code, "Error retrieving notes");
+    }
+};
 
 const getNote = async (req, res) => {
-  try {
-    /**
-     * @todo Write your code here
-     */
-  } catch (error) {
-    return apiResponse(res, responses.error.code, 'Error retrieving note')
-  }
-}
+    try {
+        const noteId = req.params.id;
+        const note = await Note.findByPk(noteId);
+        if (!note) {
+            return apiResponse(res, responses.notFound.code, "Note not found");
+        }
+        return apiResponse(
+            res,
+            responses.success.code,
+            "Note retrieved successfully",
+            note
+        );
+    } catch (error) {
+        return apiResponse(res, responses.error.code, "Error retrieving note");
+    }
+};
 
 const createNote = async (req, res) => {
-  try {
-    /**
-     * @todo Write your code here
-     */
-  } catch (error) {
-    return apiResponse(res, responses.error.code, 'Error creating note')
-  }
-}
+    try {
+        const { title, content } = req.body;
+        const newNote = await Note.create({ title, content });
+        return apiResponse(
+            res,
+            responses.created.code,
+            "Note created successfully",
+            newNote
+        );
+    } catch (error) {
+        return apiResponse(res, responses.error.code, "Error creating note");
+    }
+};
 
 const updateNote = async (req, res) => {
-  try {
-    /**
-     * @todo Write your code here
-     */
-  } catch (error) {
-    return apiResponse(res, responses.error.code, 'Error updating note')
-  }
-}
+    try {
+        const noteId = req.params.id;
+        const { title, content } = req.body;
+        const note = await Note.findByPk(noteId);
+        if (!note) {
+            return apiResponse(res, responses.notFound.code, "Note not found");
+        }
+        await note.update({ title, content });
+        return apiResponse(
+            res,
+            responses.success.code,
+            "Note updated successfully",
+            note
+        );
+    } catch (error) {
+        return apiResponse(res, responses.error.code, "Error updating note");
+    }
+};
 
 const deleteNote = async (req, res) => {
-  try {
-    /**
-     * @todo Write your code here
-     */
-  } catch (error) {
-    return apiResponse(res, responses.error.code, 'Error deleting note')
-  }
-}
+    try {
+        const noteId = req.params.id;
+        const note = await Note.findByPk(noteId);
+        if (!note) {
+            return apiResponse(res, responses.notFound.code, "Note not found");
+        }
+        await note.destroy();
+        return apiResponse(
+            res,
+            responses.success.code,
+            "Note deleted successfully",
+            { id: noteId }
+        );
+    } catch (error) {
+        return apiResponse(res, responses.error.code, "Error deleting note");
+    }
+};
 
 module.exports = {
-  getNotes,
-  getNote,
-  createNote,
-  updateNote,
-  deleteNote
-}
+    getNotes,
+    getNote,
+    createNote,
+    updateNote,
+    deleteNote,
+};
